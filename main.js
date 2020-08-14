ec7eb8bc89e2ba1debb7f290a46022e8088bc76e
Jakub Sapko <jakub.sapko1@gmail.com>
2020-08-14 22:05:28 +0200

    Threw away an idea of using particlesJS. Finished technologies section. Changed navbar behaviour and styling

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("napis").style.visibility ="hidden";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  setTimeout(function(){
+    document.getElementById("napis").style.visibility ="visible";
+  }, 250);
 }
-
-window.onload = function() {
-  Particles.init({
-    selector: '.background'
-  });
-};
